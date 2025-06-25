af7e1f917b3bd5a4a9e3a9aea5ff84f50349089c
calum <calum@quinns.ch>
2025-06-25 23:06:10 +0200

    Updates

--- _app/immutable/chunks/CRKBLMcU.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_10lz6ly)==null?void 0:s.base)??"/mangahive";var a;const l=((a=globalThis.__sveltekit_10lz6ly)==null?void 0:a.assets)??e;export{l as a,e as b};
