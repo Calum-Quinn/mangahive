d1929dde3be0a80508622fab9ca5f8160bfb9eb8
calum <calum@quinns.ch>
2025-06-25 17:29:07 +0200

    Set up deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +9,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : preprocessCSS.env.BASE_PATH,
+			base: dev ? '' : '/mangahive',
 		}
 	}
 };
